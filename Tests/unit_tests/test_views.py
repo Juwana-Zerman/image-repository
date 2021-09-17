7ce956953d50e599c3c3e0f6d1d2a5c3293ea888
Juwana Zerman <38802201+Juwana-Zerman@users.noreply.github.com>
2021-09-16 23:29:43 -0400

    Update test_views.py

--- Tests/unit_tests/test_views.py
+++ Tests/unit_tests/test_views.py
@@ -22,23 +22,28 @@ django.setup()
 class ViewTest(TestCase):
     """Tests for the application views."""
 
-     # if django.VERSION[:2] >= (3, 7):
+    if django.VERSION >= 3.*:
          # Django 1.7 requires an explicit setup() when running tests in PTVS
     @classmethod
-    def setUpClass(cls):
-        super(ViewTest, cls).setUpClass()
+#     def setUpClass(cls):
+#         super(ViewTest, cls).setUpClass()
+    def setUp(self):
+        pass
 
     def test_unit_home(self):
         """Tests the home page."""
         response = self.client.get('/')
         self.assertContains(response, 'Home Page', 1, 200)
+        pass
 
     def test_unit_contact(self):
         """Tests the contact page."""
         response = self.client.get('/contact')
         self.assertContains(response, 'Contact', 3, 200)
+        pass
 
     def test_unit_about(self):
         """Tests the about page."""
         response = self.client.get('/about')
         self.assertContains(response, 'About', 3, 200)
+        pass
