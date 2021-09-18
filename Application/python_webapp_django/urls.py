5465429e39d440b33d9e3dd81965474be87b06a4
Juwana Zerman <juwanazerman@gmail.com>
2021-09-17 23:36:50 -0400

    Update urls.py

--- Application/python_webapp_django/urls.py
+++ Application/python_webapp_django/urls.py
@@ -8,6 +8,7 @@ import django.contrib.auth.views
 from django.views.static import serve
 from django.conf import settings
 
+from django.urls import path, include
 import app.forms
 import app.views
 
@@ -21,24 +22,24 @@ urlpatterns = [
     url(r'^$', app.views.home, name='home'),
     url(r'^contact$', app.views.contact, name='contact'),
     url(r'^about', app.views.about, name='about'),
-    url(r'^login/$',
-        django.contrib.auth.views.login,
-        {
-            'template_name': 'login.html',
-            'authentication_form': app.forms.BootstrapAuthenticationForm,
-            'extra_context':
-            {
-                'title': 'Log in',
-                'year': datetime.now().year,
-            }
-        },
-        name='login'),
-    url(r'^logout$',
-        django.contrib.auth.views.logout,
-        {
-            'next_page': '/',
-        },
-        name='logout'),
+    url(r'^login/$', app.views.login, name='login'),
+    #     django.contrib.auth.views.login,
+    #     {
+    #         'template_name': 'login.html',
+    #         'authentication_form': app.forms.BootstrapAuthenticationForm,
+    #         'extra_context':
+    #         {
+    #             'title': 'Log in',
+    #             'year': datetime.now().year,
+    #         }
+    #     },
+    #     name='login'),
+    # url(r'^logout$',
+    #     django.contrib.auth.views.logout,
+    #     {
+    #         'next_page': '/',
+    #     },
+    #     name='logout'),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
